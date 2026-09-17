2c5788ca43ed27398c38404712678cca32f6b7d0
sentore1 <abdousentore@gmail.com>
2026-09-17 11:49:05 +0200

    dadad

--- app/components/CulturalEncounters.tsx
+++ app/components/CulturalEncounters.tsx
@@ -7,9 +7,8 @@ export default function CulturalEncounters() {
   const [currentSlide, setCurrentSlide] = useState(0);
 
   const sliderImages = [
-    "/images/1.jpg",
+    "/images/culture/erwrwer.webp",
     "/images/culture/a01e05166009d63863bd4fbf280359d9.jpg",
-    "/images/culture/Image.webp",
     "/images/culture/oonh-resort-tea-lounge-hallway-3.webp",
     "/images/culture/phildsc4041.jpg",
   ];
